3d56ff72c36af15a41640ce72887084595a93337
Damon Oehlman <damon.oehlman@gmail.com>
2014-01-09 14:30:43 +1100

    Added configuration for chrome beta

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -359,7 +359,13 @@ awful.rules.rules = {
       properties = {
         tag = tags[mouse.screen][3],
         floating = false
-      } }
+      } },
+    { rule = { class = "Google-chrome-beta" },
+      properties = {
+        tag = tags[mouse.screen][3],
+        floating = false
+      } },
+    
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
