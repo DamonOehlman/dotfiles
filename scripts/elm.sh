02691132d52a9c0df2d569a52c9e13c1afb2aefc
Damon Oehlman <damon.oehlman@gmail.com>
2016-01-18 16:23:07 +1100

    report elm version

--- scripts/elm.sh
+++ scripts/elm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ELM_HOME=$(dirname $(dirname $(which node)))/lib/node_modules/elm/share
 hash elm 2> /dev/null || npm install -g elm
-hash elm && echo "elm:    $(elm-make --help | head -n1 | cut -f5 -d' ' | rev | cut -c2- | rev)"
+hash elm && echo "elm:    $(elm make -h | head -n1 | cut -f2 -d' ')"
