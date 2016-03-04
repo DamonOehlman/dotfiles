5e7dfd4abad0c1871f77a9ae525400d9666a782a
Damon Oehlman <damon.oehlman@gmail.com>
2016-03-04 15:55:42 +1100

    intellij tuning

---
+++ scripts/editors/intellij.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+INTELLIJ_VERSION=15
+INTELLIJ_CONFIG_HOME=~/Library/Preferences/IntelliJIdea${INTELLIJ_VERSION}/
+
+ln -sf ${DOTFILES_HOME}/config/intellij/idea.vmoptions ${INTELLIJ_CONFIG_HOME}
