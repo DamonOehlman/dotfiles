a3904d4c9db28dd43dfec89bd45b131b5b2333d0
Damon Oehlman <damon.oehlman@gmail.com>
2016-04-15 10:58:53 +1000

    include current python version bin path

--- scripts/python.sh
+++ scripts/python.sh
@@ -2,3 +2,5 @@
 [[ -e ~/.pyenv/bin/pyenv ]] && eval "$(pyenv init -)" && eval "$(pyenv virtualenv-init -)"
 hash pyenv && pyenv global ${PYTHON_VERSION-2.7.9}
 hash python && echo "python: $(python --version 2>&1 | cut -d' ' -f2)"
+
+export PATH="~/.pyenv/versions/$(python --version 2>&1 | cut -d' ' -f2)/bin:$PATH"
