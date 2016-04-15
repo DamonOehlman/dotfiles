2280677c8f86dbbb30b55820328ee1037f5a55db
Damon Oehlman <damon.oehlman@gmail.com>
2016-04-15 10:59:21 +1000

    upgrade ruby version

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 [[ -e ~/.rvm/scripts/rvm ]] && . ~/.rvm/scripts/rvm
-hash rvm && rvm use ${RUBY_VERSION-2.2.2} &> /dev/null
+hash rvm && rvm use ${RUBY_VERSION-2.3.0} &> /dev/null
 hash ruby && echo "ruby:   $(ruby --version | cut -d' ' -f2)"
