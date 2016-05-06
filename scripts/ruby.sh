4b800cd087e7b93e29fce12981b8368500e26c82
Damon Oehlman <damon.oehlman@gmail.com>
2016-05-06 15:41:40 +1000

    tweak scripts

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -2,3 +2,6 @@
 [[ -e ~/.rvm/scripts/rvm ]] && . ~/.rvm/scripts/rvm
 hash rvm && rvm use ${RUBY_VERSION-2.3.0} &> /dev/null
 hash ruby && echo "ruby:   $(ruby --version | cut -d' ' -f2)"
+
+# some common ruby tools
+hash termrc 2> /dev/null || gem install termrc
