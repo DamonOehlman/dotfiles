c9faac5493d030399ec8386f9cb184a2ae6161e7
Damon Oehlman <damon.oehlman@gmail.com>
2016-07-13 16:54:38 +1000

    update intellij version

--- scripts/editors/intellij.sh
+++ scripts/editors/intellij.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-INTELLIJ_VERSION="2016.1"
+INTELLIJ_VERSION="2016.2"
 INTELLIJ_CONFIG_HOME=~/Library/Preferences/IntelliJIdea${INTELLIJ_VERSION}
 
 ln -sf ${DOTFILES_HOME}/config/intellij/idea.vmoptions ${INTELLIJ_CONFIG_HOME}
+
+mkdir -p ${INTELLIJ_CONFIG_HOME}/keymaps
+ln -sf ${DOTFILES_HOME}/config/intellij/keymaps/djo-Darwin.xml ${INTELLIJ_CONFIG_HOME}/keymaps/djo.xml
