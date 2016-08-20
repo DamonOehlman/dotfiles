0d46ce7527da6501e866864718869fbe738e29b0
Damon Oehlman <damon.oehlman@gmail.com>
2016-08-20 14:43:55 +1000

    Add budo into the mix for tools

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -5,3 +5,7 @@ hash eslint 2> /dev/null || npm install -g eslint
 
 # report the eslint version (and hack around the node stdout streaming)
 hash eslint && echo "eslint: $(echo $(eslint --version) | cut -c 2-)"
+
+# install budo if not available
+hash budo 2> /dev/null || npm install -g budo
+hash budo && echo "$(budo --version)"
