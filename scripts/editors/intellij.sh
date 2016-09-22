8d5ea81bf8ef7015e365c41cd5b67583c29e9813
Damon Oehlman <damon.oehlman@gmail.com>
2016-09-22 15:00:49 +1000

    include intellij code styles as part of config

--- scripts/editors/intellij.sh
+++ scripts/editors/intellij.sh
@@ -6,3 +6,6 @@ ln -sf ${DOTFILES_HOME}/config/intellij/idea.vmoptions ${INTELLIJ_CONFIG_HOME}
 
 mkdir -p ${INTELLIJ_CONFIG_HOME}/keymaps
 ln -sf ${DOTFILES_HOME}/config/intellij/keymaps/djo.xml ${INTELLIJ_CONFIG_HOME}/keymaps/djo.xml
+
+mkdir -p ${INTELLIJ_CONFIG_HOME}/codestyles
+ln -sf ${DOTFILES_HOME}/config/intellij/codestyles/djo.xml ${INTELLIJ_CONFIG_HOME}/codestyles/djo.xml
