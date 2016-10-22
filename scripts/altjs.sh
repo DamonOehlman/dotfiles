6cc34507bbcda1990e1309f9cc0cf2cbdbc65a8f
Damon Oehlman <damon.oehlman@gmail.com>
2016-10-22 12:27:10 +1100

    check for version matches

--- scripts/altjs.sh
+++ scripts/altjs.sh
@@ -1,10 +1,41 @@
 #!/usr/bin/env bash
 
+FLOW_TARGET_VERSION="0.33.0"
+
+ELM_HOME="$(dirname $(dirname $(which node)))"/lib/node_modules/elm/share
+ELM_TARGET_VERSION="0.17.1"
+
 echo -ne "\n${YELLOW}altJS:\t${NC}"
 
-# flow
-hash flow 2> /dev/null || npm install -g flow-bin
-hash flow && tool_available flow "flow version | sed s/[^0-9.]//g"
+installElm() {
+    local elm_version
+
+    if hash elm 2>/dev/null; then
+        elm_version="$(elm make -h | head -n1 | cut -f2 -d' ')"
+    fi
+
+    if [[ -z "${elm_version}" ]] || [[ "${elm_version}" != "${ELM_TARGET_VERSION}" ]]; then
+        echo -e "\nInstalling / Updating elm\n"
+        # npm install -g elm@"${ELM_TARGET_VERSION}"
+    fi
+
+    tool_available elm "elm make -h | head -n1 | cut -f2 -d' '"
+}
+
+installFlow() {
+    local flow_version
+
+    if hash flow 2>/dev/null; then
+        flow_version="$(flow version | sed s/[^0-9.]//g)"
+    fi
+
+    if [[ -z "${flow_version}" ]] || [[ "${flow_version}" != "${FLOW_TARGET_VERSION}" ]]; then
+        echo -e "\nInstalling / Updating flow\n"
+        # npm install -g flow-bin@"${FLOW_TARGET_VERSION}"
+    fi
+
+    tool_available flow "flow version | sed s/[^0-9.]//g"
+}
 
 # typescript
 hash tsc 2> /dev/null || npm install -g typescript@beta
@@ -14,9 +45,5 @@ hash tsc && tool_available tsc "tsc --version | cut -f2 -d' '" typescript
 hash haxe 2> /dev/null || npm install -g haxe
 tool_available haxe "haxe | head -n1 | cut -f3 -d' '"
 
-# elm
-ELM_HOME=$(dirname $(dirname $(which node)))/lib/node_modules/elm/share
-hash elm 2> /dev/null || npm install -g elm
-tool_available elm "elm make -h | head -n1 | cut -f2 -d' '"
-
-
+installFlow
+installElm
