d282a9dab0a0b3324df915238d78c9ac7f775fb4
Damon Oehlman <damon.oehlman@gmail.com>
2016-11-23 11:45:21 +1100

    upgrade intellij version

--- scripts/editors/intellij.sh
+++ scripts/editors/intellij.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-INTELLIJ_VERSION="2016.2"
+INTELLIJ_VERSION="2016.3"
 INTELLIJ_CONFIG_HOME=~/Library/Preferences/IntelliJIdea${INTELLIJ_VERSION}
 
 ln -sf ${DOTFILES_HOME}/config/intellij/idea.vmoptions ${INTELLIJ_CONFIG_HOME}
