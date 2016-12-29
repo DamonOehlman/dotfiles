8a49abaf7cc63e9cc74e41012ed99eddc2f1e177
Damon Oehlman <damon.oehlman@gmail.com>
2016-12-30 09:31:38 +1100

    add mert to list of tools

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -4,3 +4,6 @@ echo -ne "\n${YELLOW}tools:\t${NC}"
 
 hash eslint 2> /dev/null || npm install -g eslint
 tool_available eslint "echo $(eslint --version) | cut -c 2-"
+
+hash mert 2> /dev/null || npm install -g mert
+tool_available mert "mert --version"
