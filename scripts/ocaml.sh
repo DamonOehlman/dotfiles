8617b8c220896c0486e834250aaafd8db0270e9b
Damon Oehlman <damon.oehlman@gmail.com>
2017-03-09 11:54:01 +1100

    remove manual ocaml configuration - just use homebrew

--- scripts/ocaml.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-[[ -e ~/.opam/opam-init/init.sh ]] && .opam/opam-init/init.sh > /dev/null 2> /dev/null || true
-hash opam && eval `opam config env`
-hash opam && opam switch ${OCAML_VERSION-4.02.3}
-tool_available ocaml "ocaml -vnum"
-
-#export PATH="~/.pyenv/versions/$(python --version 2>&1 | cut -d' ' -f2)/bin:$PATH"
