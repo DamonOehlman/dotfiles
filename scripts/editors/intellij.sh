758e0d7e55d1595601084451b5ef4c273af4e9c6
Damon Oehlman <damon.oehlman@gmail.com>
2017-06-14 15:25:01 +1000

    do not attempt to configure intellj if not present

--- scripts/editors/intellij.sh
+++ scripts/editors/intellij.sh
@@ -2,10 +2,12 @@
 INTELLIJ_VERSION="2017.1"
 INTELLIJ_CONFIG_HOME=~/Library/Preferences/IntelliJIdea${INTELLIJ_VERSION}
 
-ln -sf ${DOTFILES_HOME}/config/intellij/idea.vmoptions ${INTELLIJ_CONFIG_HOME}
+if [[ -d "${INTELLIJ_CONFIG_HOME}" ]]; then
+    ln -sf ${DOTFILES_HOME}/config/intellij/idea.vmoptions ${INTELLIJ_CONFIG_HOME}
 
-mkdir -p ${INTELLIJ_CONFIG_HOME}/keymaps
-ln -sf ${DOTFILES_HOME}/config/intellij/keymaps/djo.xml ${INTELLIJ_CONFIG_HOME}/keymaps/djo.xml
+    mkdir -p ${INTELLIJ_CONFIG_HOME}/keymaps
+    ln -sf ${DOTFILES_HOME}/config/intellij/keymaps/djo.xml ${INTELLIJ_CONFIG_HOME}/keymaps/djo.xml
 
-mkdir -p ${INTELLIJ_CONFIG_HOME}/codestyles
-ln -sf ${DOTFILES_HOME}/config/intellij/codestyles/djo.xml ${INTELLIJ_CONFIG_HOME}/codestyles/djo.xml
+    mkdir -p ${INTELLIJ_CONFIG_HOME}/codestyles
+    ln -sf ${DOTFILES_HOME}/config/intellij/codestyles/djo.xml ${INTELLIJ_CONFIG_HOME}/codestyles/djo.xml
+fi
