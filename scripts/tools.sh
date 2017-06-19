8b634c06084f2622cee6a6007011b77376694a3e
Damon Oehlman <damon.oehlman@gmail.com>
2017-06-19 17:17:22 +1000

    removing eslint from the global tools

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -21,9 +21,6 @@ installTool() {
   tool_available "${tool}" "${get_version}"
 }
 
-hash eslint 2> /dev/null || npm install -g eslint
-tool_available eslint "echo $(eslint --version) | cut -c 2-"
-
 hash mert 2> /dev/null || npm install -g mert
 tool_available mert "mert --version"
 
