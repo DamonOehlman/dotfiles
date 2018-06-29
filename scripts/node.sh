cdb67b32dc44c11298c3c0b7f3dc2aacabc6b509
Damon Oehlman <damon.oehlman@gmail.com>
2018-06-29 12:36:39 +1000

    nvm + npm prefix handling

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-[[ -e ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
-hash nvm && nvm use ${NODE_VERSION-stable} > /dev/null
+NVM_ENABLED="${USE_NVM:-}"
+if [[ -e ~/.nvm/nvm.sh && "${NVM_ENABLED}" ]]; then
+  . ~/.nvm/nvm.sh 2> /dev/null
+  nvm use default > /dev/null
+  npm config delete prefix
+fi
+
 hash node && tool_available "node" "node --version | cut -c2-"
 
 # alias upgrade_yarn="$(nvm which node | sed s_/bin/node__)/lib/node_modules/npm/bin/npm-cli.js install -g yarn@latest -g"
 # alias npm="yarn $@"
 
 # make sure we have yarn
-hash yarn 2> /dev/null || upgrade_yarn
+hash yarn 2> /dev/null || npm install -g yarn
