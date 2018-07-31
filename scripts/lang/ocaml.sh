712d2a4b989ffce998b69c12f27db68e40f6611d
Damon Oehlman <damon.oehlman@gmail.com>
2018-07-31 18:56:57 +1000

    add ocaml config script

---
+++ scripts/lang/ocaml.sh
@@ -0,0 +1,8 @@
+# #!/usr/bin/env bash
+
+# opam initialisation (ocaml)
+if [[ -e "${HOME}/.opam/opam-init/init.sh" ]]; then
+  "${HOME}"/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
+
+  hash ocaml && tool_available "ocaml" "ocaml --version | cut -f5 -d' '"
+fi
