9f45de707960aea52a5bfda9101e9d7fc1e1a41f
Damon Oehlman <damon.oehlman@gmail.com>
2024-01-03 08:55:26 +1000

    .

--- Makefile
+++ Makefile
@@ -40,10 +40,6 @@ bashrc:
 configfiles:
 	@mkdir -p ~/.config
 
-synapse:
-	@rm -rf ~/.config/synapse
-	@ln -s $(DOTFILES_HOME)/config/synapse ~/.config/synapse
-
 vscode: node_modules
 	@echo "updating vscode settings in: $(PATH_VSCODE)"
 	@mkdir -p "$(PATH_VSCODE)"
@@ -103,12 +99,6 @@ ifeq ($(IS_WINDOWS),1)
 else
 	@ln -sf $(DOTFILES_HOME)/config/alacritty/alacritty.toml ~/.config/alacritty.toml
 endif
-
-bspwm:
-	@ln -sf $(DOTFILES_HOME)/config/bspwm ~/.config/
-	@ln -sf $(DOTFILES_HOME)/config/sxhkd ~/.config/
-	@ln -sf $(DOTFILES_HOME)/config/yabar ~/.config/
-	@ln -sf $(DOTFILES_HOME)/config/rofi ~/.config/
 
 tmux:
 	@DOTFILES_HOME=$(DOTFILES_HOME) $(DOTFILES_HOME)/scripts/configure-tmux.sh
--- scripts/displays.sh
+++ scripts/displays.sh
@@ -1,16 +1,4 @@
 # #!/usr/bin/env bash
 if [[ $UNAME -ne 'Darwin' ]]; then
-  # video modes
-  VM_U2713HM="U2713HM"
-  VM_U2713HM_MODELINE="172.80 1920 2040 2248 2576  1080 1081 1084 1118 -hsync +vsync"
-  VM_WORK=$VM_U2713HM
-  VM_SURFACE_SCALED="109.50  1368 1448 1592 1816  768 771 781 805 -hsync +vsync"
-  VM_B2440="172.80  1920 2040 2248 2576  1080 1081 1084 1118  -hsync +vsync"
-  DISPLAY_INTERNAL=`xrandr | sed -n 2p | cut -f1 -d$' '`
-
-  # define some video modes for different monitor configurations
-  xrandr --newmode "$VM_U2713HM" $VM_U2713HM_MODELINE &> /dev/null
-  xrandr --newmode "INTERNAL_SCALED" $VM_SURFACE_SCALED &> /dev/null
-  xrandr --newmode "VM_B2440" $VM_B2440 &> /dev/null
 fi
 
