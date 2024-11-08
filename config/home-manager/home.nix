ac2f78865b52cb07756bc977a2d9a32560092401
Damon Oehlman <damon.oehlman@gmail.com>
2024-11-08 22:43:58 +1100

    formatting

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -1,61 +1,90 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
-   home.username = "damo";
-   home.homeDirectory = "/home/damo";
-   home.stateVersion = "24.05";
-   home.packages = [
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      # Add additional package names here
+      "vscode"
+    ];
+
+  home.username = "damo";
+  home.homeDirectory = "/home/damo";
+  home.stateVersion = "24.05";
+  home.packages = [
+    # nixos things
+    pkgs.home-manager
+    pkgs.nixfmt-rfc-style
+    # hyprland things
+    pkgs.waybar
+    # cli things
     pkgs.gnupg
     pkgs.pinentry.curses
     pkgs.pass
-    pkgs.home-manager
-    pkgs.waybar
     pkgs.gnumake
+    pkgs.graphviz
+    # fonts
     pkgs.font-awesome
     pkgs.fira-code
+    # python + packages
     (pkgs.python3.withPackages (ppkgs: [
-        ppkgs.json5 ppkgs.requests
+      ppkgs.json5
+      ppkgs.requests
     ]))
-   ];
+  ];
 
-   fonts.fontconfig.enable = true;
+  fonts.fontconfig.enable = true;
 
-   programs.zsh = {
-        enable = true;
-        enableCompletion = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
 
-        shellAliases = {
-            ll = "ls -l";
-        };
-        history = {
-            size = 10000;
-            path = "${config.xdg.dataHome}/zsh/history";
-        };
-
-        oh-my-zsh = {
-            enable = true;
-            plugins = [ "git" ];
-            theme = "robbyrussell";
-        };
+    shellAliases = {
+      ll = "ls -l";
+    };
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
     };
 
-    programs.git = {
-        enable = true;
-        userName = "Damon Oehlman";
-        userEmail = "damon.oehlman@gmail.com";
-    };
-
-    services.mbsync.enable = true;
-
-    programs.gpg.enable = true;
-    services.gpg-agent = {
+    oh-my-zsh = {
       enable = true;
-      defaultCacheTtl = 34560000;
-      maxCacheTtl = 34560000;
-      pinentryPackage = pkgs.pinentry.curses;
-      enableScDaemon = false;
-      enableSshSupport = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
     };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Damon Oehlman";
+    userEmail = "damon.oehlman@gmail.com";
+  };
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      yzhang.markdown-all-in-one
+    ];
+  };
+
+  services.mbsync.enable = true;
+
+  programs.gpg.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 34560000;
+    maxCacheTtl = 34560000;
+    pinentryPackage = pkgs.pinentry.curses;
+    enableScDaemon = false;
+    enableSshSupport = true;
+  };
 }
